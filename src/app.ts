ebcea962d8ed45a5d383b802f0a9ccd9079e146f
Sri2103 <damasriharsha@gmail.com>
2023-06-03 16:19:45 -0400

    refactor: 💡 added env vars

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,20 @@
-import * as http from 'http'
-import expressServer from './server'
-
+import * as http from 'http';
+import expressServer from './server';
+import 'dotenv/config';
 class Main {
-    private server: http.Server
-    private port = 6000
+  private server: http.Server;
+  private port = 6000;
 
-    constructor(){
-        this.server = http.createServer(expressServer)
-        this.startServer()
-    }
+  constructor() {
+    this.server = http.createServer(expressServer);
+    this.startServer();
+  }
 
-    private startServer(){
-        this.server.listen(this.port, async() => {
-            console.log(`Server is running on port ${this.port}`)
-        })
-    }
+  private startServer() {
+    this.server.listen(this.port, async () => {
+      console.log(`Server is running on port ${this.port}`);
+    });
+  }
 }
 
-new Main()
+new Main();
